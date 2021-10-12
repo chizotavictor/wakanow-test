8171befe43c67a95ea9af7d4d4572440490373d1
Ebuka Chizota <chizotavictor@gmail.com>
2021-10-12 02:31:34 +0100

    1st move

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCot3nWEhTN_gBXp6OWRC3SjbvVtzeYNx8",
+    authDomain: "wakanow-3615f.firebaseapp.com",
+    projectId: "wakanow-3615f",
+    storageBucket: "wakanow-3615f.appspot.com",
+    messagingSenderId: "724610845030",
+    appId: "1:724610845030:web:949d7fca56b633cb11e0a7",
+    measurementId: "G-Z6ZP31M2KS"
+  }
 };
